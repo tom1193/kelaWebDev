3c5ff2caeca49bdf8d91a6b33d37e9ab10aa2b08
tom1193 <thomas8289@gmail.com>
2017-06-21 10:29:56 -0500

    team and news updates

--- js/main.js
+++ js/main.js
@@ -6,35 +6,28 @@ const mapLinktoDiv = function(className) {
 }
 // https://css-tricks.com/snippets/jquery/smooth-scrolling/
 $('a[href*="#"]')
-  // Remove links that don't actually link to anything
   .not('[href="#"]')
   .not('[href="#0"]')
   .click(function(event) {
-    // On-page links
     if (
       location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
       && 
       location.hostname == this.hostname
     ) {
-      // Figure out element to scroll to
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Does a scroll target exist?
       if (target.length) {
-        // Only prevent default if animation is actually gonna happen
         event.preventDefault();
         $('html, body').animate({
           scrollTop: target.offset().top
         }, 1000, function() {
-          // Callback after animation
-          // Must change focus!
           var $target = $(target);
           $target.focus();
-          if ($target.is(":focus")) { // Checking if the target was focused
+          if ($target.is(":focus")) {
             return false;
           } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
+            $target.attr('tabindex','-1'); 
+            $target.focus(); 
           };
         });
       }
@@ -42,10 +35,8 @@ $('a[href*="#"]')
   });
 $(document).ready(function(){
 
-  // apply link to all divs. change number based on # of divs
-  for (var i=0; i<7; i++){
-    mapLinktoDiv(`.press${i}`);
-  }
+  // apply link to all divs
+  mapLinktoDiv(`.press-link`);
     //apply opaque nav when mobile
   if($(window).width() <= 768){
     $('.navbar').addClass('opaque');
